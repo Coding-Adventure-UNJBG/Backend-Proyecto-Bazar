b80970b3d5fbcf354c2056ab89ee3f9e3642574b
DennisCa-UNJBG <dscalderonm@unjbg.edu.pe>
2023-05-16 18:58:33 -0500

    Generando la base de datos - Parte 03 - Avance al 88%

--- DB_backup/dbBazar.sql
+++ DB_backup/dbBazar.sql
@@ -79,10 +79,10 @@ CREATE TABLE `proveedor`(
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Table structure for table `compra` */
-DROP TABLE IF EXISTS `proveedor`;
+DROP TABLE IF EXISTS `compra`;
 
-CREATE TABLE `proveedor`(
-  `id_compra` INTEGER  AUTO_INCREMENT,
+CREATE TABLE `compra`(
+  `id_compra` INTEGER NOT NULL AUTO_INCREMENT,
   `id_proveedor` SMALLINT NULL,
   `numero_comprobante` VARCHAR(16) NOT NULL,
   `descripcion` TEXT NULL,
@@ -96,28 +96,107 @@ CREATE TABLE `proveedor`(
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Table structure for table `producto` */
-DROP TABLE IF EXISTS `proveedor`;
+DROP TABLE IF EXISTS `producto`;
 
 CREATE TABLE `producto`(
-  `id_producto` INTEGER  AUTO_INCREMENT,
+  `id_producto` SMALLINT NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(32) NOT NULL,
   `medida` VARCHAR(32) NOT NULL,
   `marca` VARCHAR(32) NOT NULL,
-  `estado` enum('activado','desactivado') NOT NULL DEFAULT 'desactivado',
-  `descripcion` TEXT NULL,
-  `importe_total` numeric(9,4) NOT NULL,
-  `costo_flete` numeric(9,4) NOT NULL,
-  `comision_banco` numeric(9,4) NOT NULL,
+  `tipo_unidad` enum('paquete','unidad') NOT NULL DEFAULT 'unidad',
+  `cantidad_unidad` TINYINT NULL,
+  `precio_sugerido` numeric(9,4) NOT NULL,
+  `estado` enum('disponible','no-disponible') NOT NULL DEFAULT 'no-disponible',
+  `stock` SMALLINT NOT NULL,
   `fecha` DATETIME NOT NULL DEFAULT current_timestamp(),
   `comentario` TINYTEXT DEFAULT '',
-  PRIMARY KEY (`id_compra`),
-  FOREIGN key (`id_proveedor`) REFERENCES `proveedor`(`id_proveedor`)
+  PRIMARY KEY (`id_producto`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
-/*Table structure for table `xxxx` */
-/*Table structure for table `xxxx` */
-/*Table structure for table `xxxx` */
-/*Table structure for table `xxxx` */
+/*Table structure for table `detalle_compra` */
+DROP TABLE IF EXISTS `detalle_compra`;
 
+CREATE TABLE `detalle_compra`(
+  `id_compra` INTEGER  NOT NULL,
+  `id_producto` SMALLINT NOT NULL,
+  `cantidad` integer(5) NOT NULL,
+  `precio_bruto` numeric(9,4) NOT NULL,
+  `comentario` TINYTEXT DEFAULT '',
+  FOREIGN KEY (`id_producto`) REFERENCES `producto`(`id_producto`),
+  FOREIGN KEY (`id_compra`) REFERENCES `compra`(`id_compra`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Table structure for table `cliente` */
+DROP TABLE IF EXISTS `cliente`;
+CREATE TABLE `cliente`(
+  `id_cliente` INTEGER(6) NOT NULL AUTO_INCREMENT,
+  `dni_ruc` integer(12) NOT NULL,
+  `nombre` TEXT NULL,
+  `direccion` TEXT NOT NULL,
+  `comentario` TINYTEXT DEFAULT '',
+  PRIMARY KEY (`id_cliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Table structure for table `venta` */
+DROP TABLE IF EXISTS `venta`;
+CREATE TABLE `venta`(
+  `id_venta` INTEGER(6) NOT NULL AUTO_INCREMENT,
+  `id_cliente` INTEGER(6) NOT NULL,
+  `tipo_comprobante` enum('boleta','factura') NOT NULL DEFAULT 'boleta',
+  `correlativo` integer(4) NOT NULL,
+  `serie` integer(6) NOT NULL,
+  `fecha` DATETIME NOT NULL DEFAULT current_timestamp(),
+  `fecha_mod` DATETIME NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `tipo_pago` enum('efectivo','yape') NOT NULL DEFAULT 'efectivo',
+  `comentario` TINYTEXT DEFAULT '',
+  PRIMARY KEY (`id_venta`),
+  FOREIGN KEY (`id_cliente`) REFERENCES `cliente`(`id_cliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Table structure for table `detalle_venta` */
+DROP TABLE IF EXISTS `detalle_venta`;
+
+CREATE TABLE `detalle_venta`(
+  `id_venta` INTEGER  NOT NULL,
+  `id_producto` SMALLINT NOT NULL,
+  `cantidad` integer(5) NOT NULL,
+  `costo_unitario` numeric(9,4) NOT NULL,
+  `comentario` TINYTEXT DEFAULT '',
+  FOREIGN KEY (`id_venta`) REFERENCES `venta`(`id_venta`),
+  FOREIGN KEY (`id_producto`) REFERENCES `producto`(`id_producto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Table structure for table `caja` */
+DROP TABLE IF EXISTS `caja`;
+
+CREATE TABLE `caja`(
+  `id_caja` TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `saldo` numeric(9,4),
+  `comentario` TINYTEXT DEFAULT '',
+  PRIMARY KEY (`id_caja`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Table structure for table `detalle_caja` */
+DROP TABLE IF EXISTS `detalle_caja`;
+CREATE TABLE `detalle_caja`(
+  `id_caja` INTEGER  NOT NULL,
+  `id_detalle` SMALLINT NOT NULL,
+  `fecha` DATETIME NOT NULL DEFAULT current_timestamp(),
+  `saldo_anterior` numeric(9,4) NOT NULL,
+  `tipo_mov` enum('venta','compra','gasto','ingreso') NOT NULL DEFAULT 'venta',
+  `comentario` TINYTEXT DEFAULT '',
+  FOREIGN KEY (`id_caja`) REFERENCES `caja`(`id_caja`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Table structure for table `logs` */
+DROP TABLE IF EXISTS `logs`;
+
+CREATE TABLE `logs`(
+  `id_registro` INT(6) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nombre_usuario` VARCHAR(32) NOT NULL,
+  `saldo` numeric(9,4),
+  `comentario` TINYTEXT DEFAULT '',
+  PRIMARY KEY (`id_caja`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
