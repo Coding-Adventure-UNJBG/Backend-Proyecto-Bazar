fa6c2e792684b43a8bf613a5c0b62528782658c2
DennisCalderon <dennis.tacna@gmail.com>
2023-05-16 09:59:30 -0500

    Mejorando las consultas en el modelo CIUDAD

--- src/model/ciudad.js
+++ src/model/ciudad.js
@@ -2,40 +2,43 @@ const model= {};
 import sequelize from '../config/db.js';
 
 model.mostrarTodo = () => {
-    return new Promise((resolve, reject) => {
-        sequelize.query('SELECT * FROM country', { raw: true })
-        .then(([result]) => resolve(result))
-        .catch((error) => reject(error));
-    });
+    return sequelize.query('SELECT * FROM country', { raw: true })
+        .then(([result, metadata]) => {
+            //console.log(metadata);
+            return result;
+        })
+        .catch((error) => { throw error });
 };
 
 model.buscarId = (id) => {
-    return new Promise((resolve, reject) => {
-        sequelize.query('SELECT * FROM country where country_id = ?', {
-            replacements: [id],
-            raw: true
+    const query = `SELECT * FROM country where country_id ='${id}'`;
+    return sequelize.query(query , { raw: true })
+        .then(([result, metadata]) => {
+            //console.log(metadata);
+            return result;
         })
-        .then(([result]) => resolve(result))
-        .catch((error) => reject(error));
-    });
+        .catch((error) => { throw error });
 };
 
 model.insertar = (data) => {
-    return new Promise((resolve, reject) => {
-        const { name, coment } = data;
-        const query = `INSERT INTO country (country, coment) VALUES ('${name}', '${coment}')`;
-        sequelize.query(query, { raw: true })
-        .then((result) => { resolve(result) })
-        .catch((error) => { reject(error) });
-    });
+    const { name, coment } = data;
+    const query = `INSERT INTO country (country, coment) VALUES ('${name}', '${coment}')`;
+    return sequelize.query(query, { raw: true })
+        .then(([result, metadata]) => {
+            //console.log(metadata);
+            return result;
+        })
+        .catch((error) => { throw error });
 };
 
 model.update = (id, name) => {
-    // update ya devuelve una promesa por lo mismo no es necesario crear una
     const query = `UPDATE country SET country = "${name}" WHERE country_id = ${id}`;
     return sequelize.query(query, { raw: true })
-    .then(([result]) => result)
-    .catch((error) => { throw error });
+        .then(([result, metadata]) => {
+            //console.log(metadata);
+            return result;
+        })
+        .catch((error) => { throw error });
 };
 
 export default model;
