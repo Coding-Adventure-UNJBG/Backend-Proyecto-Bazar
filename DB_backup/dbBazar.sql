b4e7ae4bf80d951063af64c1ad556e3123dbf764
DennisCa-UNJBG <dscalderonm@unjbg.edu.pe>
2023-06-15 17:41:22 -0500

    Se agregó la tabla temporal precio_producto y se añadieron algunas restricciones

--- DB_backup/dbBazar.sql
+++ DB_backup/dbBazar.sql
@@ -12,9 +12,9 @@ DROP TABLE IF EXISTS `usuario`;
 
 CREATE TABLE `usuario` (
   `id_usuario` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `cuenta` VARCHAR(16) NOT NULL,
+  `cuenta` VARCHAR(16) NOT NULL UNIQUE,
   `password` VARCHAR(32) NOT NULL,
-  `dni` integer(10) NOT NULL,
+  `dni` integer(10) NOT NULL UNIQUE,
   `nombres` VARCHAR(64) NOT NULL,
   `telefono` VARCHAR(13) NULL,
   `direccion` TINYTEXT NULL DEFAULT '',
@@ -22,8 +22,6 @@ CREATE TABLE `usuario` (
   `fecha_registro` DATETIME NOT NULL DEFAULT current_timestamp(),
   `fecha_mod` DATETIME NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `comentario` TINYTEXT DEFAULT '',
-  UNIQUE (`dni`),
-  UNIQUE (`cuenta`),
   PRIMARY KEY(`id_usuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -69,8 +67,8 @@ CREATE TABLE `detalle_rol`(
   `is_custom` BOOLEAN DEFAULT 0,
   `comentario` TINYTEXT DEFAULT '',
   PRIMARY KEY(`id_usuario`, `id_rol`),
-  FOREIGN key (`id_usuario`) REFERENCES `usuario`(`id_usuario`),
-  FOREIGN key (`id_rol`) REFERENCES `rol`(`id_rol`)
+  FOREIGN KEY (`id_usuario`) REFERENCES `usuario`(`id_usuario`),
+  FOREIGN KEY (`id_rol`) REFERENCES `rol`(`id_rol`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `rol` */
@@ -90,7 +88,7 @@ DROP TABLE IF EXISTS `proveedor`;
 CREATE TABLE `proveedor`(
   `id_proveedor` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(32) NOT NULL,
-  `ruc` VARCHAR(32) NULL,
+  `ruc` VARCHAR(32) NULL UNIQUE,
   `razon_social` TINYTEXT NOT NULL,
   `direccion` TINYTEXT NOT NULL,
   `estado` enum('habilitado','deshabilitado') NOT NULL DEFAULT 'deshabilitado',
@@ -105,11 +103,11 @@ DROP TABLE IF EXISTS `compra`;
 CREATE TABLE `compra`(
   `id_compra` INTEGER NOT NULL AUTO_INCREMENT,
   `id_proveedor` SMALLINT UNSIGNED NOT NULL,
-  `numero_comprobante` VARCHAR(16) NOT NULL,
+  `numero_comprobante` VARCHAR(16) NOT NULL UNIQUE,
   `descripcion` TEXT NULL,
-  `importe_total` numeric(9,4) NOT NULL,
-  `costo_flete` numeric(9,4) NOT NULL,
-  `comision_banco` numeric(9,4) NOT NULL,
+  `importe_total` numeric(9,4) UNSIGNED NOT NULL,
+  `costo_flete` numeric(9,4) UNSIGNED NOT NULL,
+  `comision_banco` numeric(9,4) UNSIGNED NOT NULL,
   `fecha` DATETIME NOT NULL DEFAULT current_timestamp(),
   `comentario` TINYTEXT DEFAULT '',
   PRIMARY KEY (`id_compra`),
@@ -125,8 +123,8 @@ CREATE TABLE `producto`(
   `medida` VARCHAR(32) NOT NULL,
   `marca` VARCHAR(32) NOT NULL,
   `tipo_unidad` enum('paquete','unidad') NOT NULL DEFAULT 'unidad',
-  `cantidad_unidad` TINYINT NOT NULL DEFAULT 1,
-  `precio_sugerido` numeric(9,4) NULL DEFAULT 0,
+  `cantidad_unidad` TINYINT UNSIGNED NOT NULL DEFAULT 1,
+  `precio_sugerido` numeric(9,4) UNSIGNED NULL DEFAULT 0,
   `estado` enum('disponible','no-disponible') NOT NULL DEFAULT 'no-disponible',
   `stock` SMALLINT NOT NULL DEFAULT 0,
   `foto` VARCHAR(70) NOT NULL DEFAULT '',
@@ -151,9 +149,10 @@ DROP TABLE IF EXISTS `detalle_compra`;
 CREATE TABLE `detalle_compra`(
   `id_compra` INTEGER  NOT NULL,
   `id_producto` SMALLINT NOT NULL,
-  `cantidad` integer(5) NOT NULL,
-  `precio_bruto` numeric(9,4) NOT NULL,
+  `cantidad` integer(5) UNSIGNED NOT NULL,
+  `precio_bruto` numeric(9,4) UNSIGNED NOT NULL,
   `comentario` TINYTEXT DEFAULT '',
+  PRIMARY KEY (`id_producto`,`id_compra`),
   FOREIGN KEY (`id_producto`) REFERENCES `producto`(`id_producto`),
   FOREIGN KEY (`id_compra`) REFERENCES `compra`(`id_compra`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -191,9 +190,10 @@ DROP TABLE IF EXISTS `detalle_venta`;
 CREATE TABLE `detalle_venta`(
   `id_venta` INTEGER  NOT NULL,
   `id_producto` SMALLINT NOT NULL,
-  `cantidad` integer(5) NOT NULL,
-  `costo_unitario` numeric(9,4) NOT NULL,
+  `cantidad` integer(5) UNSIGNED NOT NULL,
+  `costo_unitario` numeric(9,4) UNSIGNED NOT NULL,
   `comentario` TINYTEXT DEFAULT '',
+  PRIMARY KEY (`id_venta`,`id_producto`),
   FOREIGN KEY (`id_venta`) REFERENCES `venta`(`id_venta`),
   FOREIGN KEY (`id_producto`) REFERENCES `producto`(`id_producto`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -217,6 +217,7 @@ CREATE TABLE `detalle_caja`(
   `saldo_anterior` numeric(9,4) NOT NULL,
   `tipo_mov` enum('venta','compra','gasto','ingreso') NOT NULL DEFAULT 'venta',
   `comentario` TINYTEXT DEFAULT '',
+  PRIMARY KEY (`id_detalle`),
   FOREIGN KEY (`id_caja`) REFERENCES `caja`(`id_caja`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -231,138 +232,14 @@ CREATE TABLE `logs`(
   PRIMARY KEY (`id_registro`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+/*Table structure for table `precio_producto` */
+DROP TABLE IF EXISTS `precio_producto`;
+CREATE TABLE `precio_producto` (
+  `id_producto` smallint(6) NOT NULL,
+  `fechaInicio` timestamp NOT NULL DEFAULT current_timestamp(),
+  `fechaFinal` timestamp NOT NULL DEFAULT current_timestamp(),
+  `precio_sugerido` decimal(9,4) UNSIGNED DEFAULT 0,
+  PRIMARY KEY (`fechaInicio`,`id_producto`),
+  FOREIGN KEY (`id_producto`) REFERENCES `producto`(`id_producto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-DROP TABLE IF EXISTS `country`;
-
-CREATE TABLE `country` (
-  `country_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-  `country` varchar(50) NOT NULL,
-  `last_update` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `coment` varchar(255) DEFAULT '',
-  PRIMARY KEY (`country_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=117 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
-/*Data for the table `country` */
-
-LOCK TABLES `country` WRITE;
-
-insert  into `country`(`country_id`,`country`,`last_update`,`coment`)
-values
-(1,'Afghanistan22','2023-05-07 15:06:30',''),
-(2,'Algeria','2006-02-15 04:44:00',''),
-(3,'American Samoa','2006-02-15 04:44:00',''),
-(4,'Angola','2006-02-15 04:44:00',''),
-(5,'Anguilla','2006-02-15 04:44:00',''),
-(6,'Argentina','2006-02-15 04:44:00',''),
-(7,'Armenia','2006-02-15 04:44:00',''),
-(8,'Australia','2006-02-15 04:44:00',''),
-(9,'Austria','2006-02-15 04:44:00',''),
-(10,'Azerbaijan','2006-02-15 04:44:00',''),
-(11,'Bahrain','2006-02-15 04:44:00',''),
-(12,'Bangladesh','2006-02-15 04:44:00',''),
-(13,'Belarus','2006-02-15 04:44:00',''),
-(14,'Bolivia','2006-02-15 04:44:00',''),
-(15,'Brazil','2006-02-15 04:44:00',''),
-(16,'Brunei','2006-02-15 04:44:00',''),
-(17,'Bulgaria','2006-02-15 04:44:00',''),
-(18,'Cambodia','2006-02-15 04:44:00',''),
-(19,'Afghanistan22','2023-05-07 15:15:57',''),
-(20,'Canada','2006-02-15 04:44:00',''),
-(21,'Chad','2006-02-15 04:44:00',''),
-(22,'Chile','2006-02-15 04:44:00',''),
-(23,'China','2006-02-15 04:44:00',''),
-(24,'Colombia','2006-02-15 04:44:00',''),
-(25,'Congo, The Democratic Republic of the','2006-02-15 04:44:00',''),
-(26,'Czech Republic','2006-02-15 04:44:00',''),
-(27,'Dominican Republic','2006-02-15 04:44:00',''),
-(28,'Ecuador','2006-02-15 04:44:00',''),
-(29,'Egypt','2006-02-15 04:44:00',''),
-(30,'Estonia','2006-02-15 04:44:00',''),
-(31,'Ethiopia','2006-02-15 04:44:00',''),
-(32,'Faroe Islands','2006-02-15 04:44:00',''),
-(33,'Finland','2006-02-15 04:44:00',''),
-(34,'France','2006-02-15 04:44:00',''),
-(35,'French Guiana','2006-02-15 04:44:00',''),
-(36,'French Polynesia','2006-02-15 04:44:00',''),
-(37,'Gambia','2006-02-15 04:44:00',''),
-(38,'Germany','2006-02-15 04:44:00',''),
-(39,'Greece','2006-02-15 04:44:00',''),
-(40,'Greenland','2006-02-15 04:44:00',''),
-(41,'Holy See (Vatican City State)','2006-02-15 04:44:00',''),
-(42,'Hong Kong','2006-02-15 04:44:00',''),
-(43,'Hungary','2006-02-15 04:44:00',''),
-(44,'India','2006-02-15 04:44:00',''),
-(45,'Indonesia','2006-02-15 04:44:00',''),
-(46,'Iran','2006-02-15 04:44:00',''),
-(47,'Iraq','2006-02-15 04:44:00',''),
-(48,'Israel','2006-02-15 04:44:00',''),
-(49,'Italy','2006-02-15 04:44:00',''),
-(50,'Japan','2006-02-15 04:44:00',''),
-(51,'Kazakstan','2006-02-15 04:44:00',''),
-(52,'Kenya','2006-02-15 04:44:00',''),
-(53,'Kuwait','2006-02-15 04:44:00',''),
-(54,'Latvia','2006-02-15 04:44:00',''),
-(55,'Liechtenstein','2006-02-15 04:44:00',''),
-(56,'Lithuania','2006-02-15 04:44:00',''),
-(57,'Madagascar','2006-02-15 04:44:00',''),
-(58,'Malawi','2006-02-15 04:44:00',''),
-(59,'Malaysia','2006-02-15 04:44:00',''),
-(60,'Mexico','2006-02-15 04:44:00',''),
-(61,'Moldova','2006-02-15 04:44:00',''),
-(62,'Morocco','2006-02-15 04:44:00',''),
-(63,'Mozambique','2006-02-15 04:44:00',''),
-(64,'Myanmar','2006-02-15 04:44:00',''),
-(65,'Nauru','2006-02-15 04:44:00',''),
-(66,'Nepal','2006-02-15 04:44:00',''),
-(67,'Netherlands','2006-02-15 04:44:00',''),
-(68,'New Zealand','2006-02-15 04:44:00',''),
-(69,'Nigeria','2006-02-15 04:44:00',''),
-(70,'North Korea','2006-02-15 04:44:00',''),
-(71,'Oman','2006-02-15 04:44:00',''),
-(72,'Pakistan','2006-02-15 04:44:00',''),
-(73,'Paraguay','2006-02-15 04:44:00',''),
-(74,'Peru','2006-02-15 04:44:00',''),
-(75,'Philippines','2006-02-15 04:44:00',''),
-(76,'Poland','2006-02-15 04:44:00',''),
-(77,'Puerto Rico','2006-02-15 04:44:00',''),
-(78,'Romania','2006-02-15 04:44:00',''),
-(79,'Runion','2006-02-15 04:44:00',''),
-(80,'Russian Federation','2006-02-15 04:44:00',''),
-(81,'Saint Vincent and the Grenadines','2006-02-15 04:44:00',''),
-(82,'Saudi Arabia','2006-02-15 04:44:00',''),
-(83,'Senegal','2006-02-15 04:44:00',''),
-(84,'Slovakia','2006-02-15 04:44:00',''),
-(85,'South Africa','2006-02-15 04:44:00',''),
-(86,'South Korea','2006-02-15 04:44:00',''),
-(87,'Spain','2006-02-15 04:44:00',''),
-(88,'Sri Lanka','2006-02-15 04:44:00',''),
-(89,'Sudan','2006-02-15 04:44:00',''),
-(90,'Sweden','2006-02-15 04:44:00',''),
-(91,'Switzerland','2006-02-15 04:44:00',''),
-(92,'Taiwan','2006-02-15 04:44:00',''),
-(93,'Tanzania','2006-02-15 04:44:00',''),
-(94,'Thailand','2006-02-15 04:44:00',''),
-(95,'Tonga','2006-02-15 04:44:00',''),
-(96,'Tunisia','2006-02-15 04:44:00',''),
-(97,'Turkey','2006-02-15 04:44:00',''),
-(98,'Turkmenistan','2006-02-15 04:44:00',''),
-(99,'Tuvalu','2006-02-15 04:44:00',''),
-(100,'Ukraine','2006-02-15 04:44:00',''),
-(101,'United Arab Emirates','2006-02-15 04:44:00',''),
-(102,'United Kingdom','2006-02-15 04:44:00',''),
-(103,'United States','2006-02-15 04:44:00',''),
-(104,'Venezuela','2006-02-15 04:44:00',''),
-(105,'Vietnam','2006-02-15 04:44:00',''),
-(106,'Virgin Islands, U.S.','2006-02-15 04:44:00',''),
-(107,'Yemen','2006-02-15 04:44:00',''),
-(108,'Yugoslavia','2006-02-15 04:44:00',''),
-(109,'Zambia','2006-02-15 04:44:00',''),
-(110,'Afghanistan22','2023-05-07 15:32:09','pruebas'),
-(111,'Afghanistan22','2023-05-07 15:32:27','pruebas'),
-(112,'Afghanistan22','2023-05-07 15:32:50','pruebas'),
-(113,'Afghanistan22','2023-05-07 15:33:58','pruebas'),
-(114,'Afghanistan22','2023-05-07 15:34:11','pruebas'),
-(115,'Afghanistan22','2023-05-07 15:37:07','pruebas'),
-(116,'Afghanistan22','2023-05-07 15:38:34','pruebas');
-
-UNLOCK TABLES;
