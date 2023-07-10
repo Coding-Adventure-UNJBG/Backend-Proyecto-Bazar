bfaadcad874f652bbd30b6e3aef240c89242863d
oscarchoq <oachoques@unjbg.edu.pe>
2023-07-10 11:30:58 -0500

    Iniciando la entrada de productos v2

--- src/controllers/entrada.js
+++ src/controllers/entrada.js
@@ -32,51 +32,6 @@ controllers.insertar = async (req, res) => {
     })
 }
 
-controllers.comprobarComprobante = async (req, res) => {
-    model.comprobarComprobante(req.query)
-    .then((data) => {
-        if (data.length >= 1) {
-            res.status(200).send({ error: 'El comprobante ya se encuentra registrado' })
-        } else {
-            res.status(200).send({ message: 'Comprobante no registrado' })
-        }
-    })
-    .catch((error) => {
-        res.status(500).send({ error: 'Error interno del servidor al obtener resultados' })
-    })
-}
-
-controllers.mostrarDetalles = async (req, res) => {
-  model.mostrarDetalle(req.query.id_compra)
-    .then((data) => {
-      if (data.length == 0) {
-        res.status(404).send({ error: 'No se encontraron resultados' })
-      }
-      else {
-        res.json(data)
-      }
-    })
-    .catch((error) => {
-      // console.log(error)
-      res.status(500).send({ error: 'Error interno del servidor al obtener resultados' })
-    })
-}
-
-controllers.insertarDetalle = async(req, res) => {
-  model.insertarDetalleCompra(req.body)
-    .then((response) => {
-      console.log("Cantidad de filas insertadas: " + response)
-      if (response >= 1) {
-        res.status(201).send({ message: 'Producto agregado correctamente' })
-      } else {
-        res.status(500).send({ error: 'Error al agregar el producto' })
-      }
-    })
-    .catch((err) => {
-      res.status(500).send({ error: 'Error interno del servidor al obtener resultados' })
-    })
-}
-
 controllers.borrarEntrada = async(req, res) => {
   model.borrarEntrada(req.body)
   .then((response) => {
--- src/model/entrada.js
+++ src/model/entrada.js
@@ -11,20 +11,9 @@ model.obtenerId = () => {
 }
 
 model.insertarCompra = (data) => {
-  const { id_compra, id_proveedor, numero_comprobante, descripcion, importe_total, costo_flete, comision_banco, fecha, comentario } = data
-  const query = `INSERT INTO compra(id_compra, id_proveedor, numero_comprobante, descripcion, importe_total, costo_flete, comision_banco, fecha, comentario)
-                    VALUES ('${id_compra}', '${id_proveedor}', '${numero_comprobante}', '${descripcion}', '${importe_total}', '${costo_flete}', '${comision_banco}', '${fecha}', '${comentario}')`
-  return sequelize.query(query, { raw: true })
-    .then(([result, metadata]) => {
-      // console.log(metadata)
-      return result
-    })
-    .catch((errror) => { throw errror })
-}
-
-model.comprobarComprobante = (data) => {
-  const { id_compra, numero_comprobante } = data
-  const query = `SELECT * FROM compra WHERE id_compra != '${id_compra}' AND numero_comprobante = '${numero_comprobante}'`
+  const { id_proveedor, id_producto, descripcion, cantidad, precio_compra, importe_total, costo_operacion } = data
+  const query = `INSERT INTO compra(id_proveedor, id_producto, descripcion, cantidad, precio_compra, importe_total, costo_operacion)
+                VALUES ('${id_proveedor}', '${id_producto}', '${descripcion}', '${cantidad}', '${precio_compra}', '${importe_total}', '${costo_operacion}')`
   return sequelize.query(query, { raw: true })
     .then(([result, metadata]) => {
       // console.log(metadata)
@@ -43,60 +32,5 @@ model.borrarEntrada = (data) => {
     })
     .catch((errror) => { throw errror })
 }
-
-
-//Detalle de compra
-
-model.mostrarDetalle = (id_compra) => {
-  const query = `SELECT d.id_compra, d.id_producto, p.nombre, p.marca, d.cantidad, ROUND(d.precio_bruto, 2) AS precio_bruto
-                FROM  detalle_compra d INNER JOIN producto p ON d.id_producto = p.id_producto
-                WHERE d.id_compra = '${id_compra}'`
-  return sequelize.query(query, { raw: true })
-    .then(([result, metadata]) => {
-      // console.log(metadata)
-      return result
-    })
-    .catch((errror) => { throw errror })
-}
-
-
-model.insertarDetalleCompra = (data) => {
-  const { id_compra, id_producto, cantidad, precio_bruto } = data
-  const query = `INSERT INTO detalle_compra(id_compra, id_producto, cantidad, precio_bruto)
-                VALUES ('${id_compra}', '${id_producto}', '${cantidad}', '${precio_bruto}')`
-  return sequelize.query(query, { raw: true })
-    .then(([result, metadata]) => {
-      return metadata;
-    })
-    .catch((error) => { throw error });
-}
-
-model.mostrarProducto = (data) => {
-  const { id_compra, id_producto } = data
-  const query = `SELECT * FROM detalle_compra WHERE '${id_compra}' AND id_producto = '${id_producto}'`
-}
-
-model.borrarProducto = (data) => {
-  const { id_compra, id_producto } = data
-  const query = `DELETE FROM detalle_compra WHERE id_compra = '${id_compra}' AND id_producto = '${id_producto}'`
-  return sequelize.query(query, { raw: true })
-    .then(([result, metadata]) => {
-      return metadata;
-    })
-    .catch((error) => { throw error });
-}
-
-model.updateProducto = (data) => {
-  const { id_compra, id_producto, cantidad, precio_bruto } = data
-  const query = `UPDATE detalle_compra SET 
-                cantidad = '${cantidad}', precio_bruto = '${precio_bruto}'
-                WHERE id_compra = '${id_compra}' AND id_producto = '${id_producto}'`
-  return sequelize.query(query, { raw: true })
-    .then(([result, metadata]) => {
-      return metadata;
-    })
-    .catch((error) => { throw error });
-}
-
 
 export default model
